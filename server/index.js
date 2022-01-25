29c514c451ee07eef30b2c68aa36478ce1f156c5
Anurag Sisodiya <anurps9@gmail.com>
2022-01-25 11:49:46 +0530

    add react

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-const PORT = process.env.PORT || 3001
+
+app.use(express.static(path.join(__dirname, '../client/build')))
 
 app.get('/', (req, res) => {
-    res.send('Hey there!')
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`);
 })
