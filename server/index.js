d6e494b1b4e414344fed4c88b44779b098b91500
Anurag Sisodiya <anurps9@gmail.com>
2022-01-25 12:18:09 +0530

    add user route and model

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const user = require('./routes/user')
 
 app.use(express.static(path.join(__dirname, '../client/build')))
+app.use('/user', user)
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
---
+++ server/routes/user.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const User = require('../models/user')
+
+router
+.get('/', (req, res) => {
+    
+})
+.get('/:id', (req, res) => {
+
+})
+.post('/', (req, res) => {
+
+})
+.put('/:id', (req, res) => {
+
+})
+.delete('/:id', (req, res) => {
+
+})
+.delete('/', (req, res) => {
+
+})
+
+module.exports = router;
